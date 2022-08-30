afe73eca02e11baf5f4644876ab60beffa8f856f
SpiritualForest <meditationlover@gmail.com>
2022-08-30 11:51:16 +0300

    Continued to develop frontend web interface.

--- src/main/resources/public/tasks.js
+++ src/main/resources/public/tasks.js
@@ -1,2 +1,10 @@
 // This JavaScript file interacts with the API using fetch()
 // and whatever else. Coming soon.
+
+/* Add task modal functions */
+
+let addTaskBtn = document.getElementById("open-modal-btn");
+let modalDiv = document.getElementById("add-task-modal");
+let cancelBtn = document.getElementById("add-task-modal-cancel-btn");
+addTaskBtn.onclick = function() { modalDiv.style.display = "block"; }
+cancelBtn.onclick = function() { modalDiv.style.display = "none"; }
