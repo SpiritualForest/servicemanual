c6ae9c10063304764c400980e99424af3efc2c68
SpiritualForest <meditationlover@gmail.com>
2022-09-02 23:01:52 +0300

    Fixed API spec. Improved commenting on QueryParameterException

--- src/main/java/com/etteplan/servicemanual/maintenancetask/QueryParameterException.java
+++ src/main/java/com/etteplan/servicemanual/maintenancetask/QueryParameterException.java
@@ -1,7 +1,7 @@
 package com.etteplan.servicemanual.maintenancetask;
 
 // This exception is thrown when our QueryResolver encounters
-// an unconvertable parameter.
+// an unconvertable or unknown query parameter.
 
 public class QueryParameterException extends Exception {
     public QueryParameterException(String message) {
