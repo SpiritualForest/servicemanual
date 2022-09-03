a3e2eb7216b247dc0022f05cca32c66a1505bef9
SpiritualForest <meditationlover@gmail.com>
2022-09-03 10:24:05 +0300

    Improved the QueryResolver parameter resolution and database resolution functions.
    Instead of checking whether each parameter is null, we assign it a specific integer value, 1, 2, or 4. The combination of these values tells us which parameters our resolveQuery() function encountered. This way our queryDatabase() function can know which JPA repository method to call, and with what parameters, based on that value. If 1: only deviceId, if 2, only status, if 3: device and status, and so forth.

--- src/main/java/com/etteplan/servicemanual/maintenancetask/QueryResolver.java
+++ src/main/java/com/etteplan/servicemanual/maintenancetask/QueryResolver.java
@@ -29,6 +29,21 @@ public final class QueryResolver {
     private static final String notConvertable = "Bad request: could not convert parameter '%s'. %s";
     private static final String availableStatus = "Available values for status: 'OPEN', 'CLOSED'";
     private static final String availableSeverity = "Available values for severity: 'UNIMPORTANT', 'IMPORTANT', 'CRITICAL'";
+    
+    /* Database Query parameters represent a combined value between 1 and 7.
+     * Based on this value, we decide which database query function to call
+     * in order to retrieve tasks.
+     * Each different query parameter has been assigned its own numerical value:
+     * deviceId: 1, status: 2, severity: 4. The combination of these values tell us
+     * which parameters we need to pass to the JPA repository function.
+     * So for example, if we encounter the deviceId and status parameters in our query,
+     * the combined value will become 3. So we know then to pass only parameters deviceId and status.
+     * This works similarly to Linux's filesystem permissions system.
+     * So if we got a total combined value of 7, it means we filter with all 3 parameters. */
+    
+    private static final int DQ_DEVICEID = 1;
+    private static final int DQ_STATUS = 2;
+    private static final int DQ_SEVERITY = 4;
 
     // We store this field because we need it for creating "/api/tasks?deviceId=<id>" hyperlinks
     // in the controller that uses this class, when responding to GET requests.
@@ -50,7 +65,8 @@ public final class QueryResolver {
             return taskRepository.findAllByOrderBySeverityAscRegistered();
         }
         // Parameters were supplied. Resolve.
-         
+        
+        int databaseMethod = 0; // Which JPA repository method to call, based on the parameters we encountered
         Long deviceId = null;
         TaskStatus status = null;
         TaskSeverity severity = null;
@@ -62,28 +78,34 @@ public final class QueryResolver {
             }
             // If we reached here, no exception was thrown.
             // Parameter is correct, validate the data
+
+            // We perform the bitwise AND operations here to ensure that we don't get
+            // multiple parameters of the same name.
             String value = parameters.get(param);
-            if (param.equals(Q_STATUS)) {
+            if (param.equals(Q_STATUS) && ((databaseMethod & DQ_STATUS) != DQ_SEVERITY)) {
                 try { 
                     status = TaskStatus.valueOf(value);
+                    databaseMethod += DQ_STATUS; // Indicate that we found the status parameter
                 }
                 catch(IllegalArgumentException ex) {
                     // Bad parameter for status. Throw exception, show which statuses are available
                     throw new QueryParameterException(String.format(notConvertable, value, availableStatus));
                 }
             }
-            else if (param.equals(Q_SEVERITY)) {
+            else if (param.equals(Q_SEVERITY) && ((databaseMethod & DQ_SEVERITY) != DQ_SEVERITY)) {
                 try {
                     severity = TaskSeverity.valueOf(value);
+                    databaseMethod += DQ_SEVERITY; // Indicate that we found the severity parameter
                 }
                 catch(IllegalArgumentException ex) {
                     // Bad parameter for severity.
                     throw new QueryParameterException(String.format(notConvertable, value, availableSeverity));
                 }
             }
-            else if (param.equals(Q_DEVICEID)) {
+            else if (param.equals(Q_DEVICEID) && ((databaseMethod & DQ_DEVICEID) != DQ_DEVICEID)) {
                 try {
                     deviceId = Long.parseLong(value);
+                    databaseMethod += DQ_DEVICEID; // Indicate that we found the deviceId parameter
                     QueryResolver.deviceId = deviceId; // Stored for hyperlink creation in the controller
                 }
                 catch(IllegalArgumentException ex) {
@@ -92,44 +114,48 @@ public final class QueryResolver {
                 }
             }
         }
-        // Query the database
-        return queryDatabase(taskRepository, deviceId, status, severity);
+        // Now we call the database query resolution function with all 3 parameters, and pass our databaseMethod variable
+        // to tell it which query it should use. Based on this, it will know which JPA repository function to call
+        // and which query parameters to pass along.
+        return queryDatabase(taskRepository, databaseMethod, deviceId, status, severity);
     }
 
-    private static List<MaintenanceTask> queryDatabase(MaintenanceTaskRepository taskRepository, Long deviceId, TaskStatus status, TaskSeverity severity) {
+    private static List<MaintenanceTask> queryDatabase(MaintenanceTaskRepository taskRepository, int method, Long deviceId, TaskStatus status, TaskSeverity severity) {
         // Query the database based on the parameters we received
+        // The method parameter contains a value between 1 and 7
+        // DQ_DEVICEID = 1, DQ_STATUS = 2, DQ_SEVERITY = 4.
+        // The combination of these values determine which database query method to call
+        // from our task repository. This way we don't have to perform null checks on parameters :)
         List<MaintenanceTask> tasks = new ArrayList<>();
-        if (deviceId == null) {
-            // Status and severity
-            if (status != null && severity != null) {
-                tasks = taskRepository.findAllByStatusAndSeverityOrderBySeverityAscRegistered(status, severity);
-            }
-            else if (status != null) {
-                // Status only
-                tasks = taskRepository.findAllByStatusOrderBySeverityAscRegistered(status);
-            }
-            else if (severity != null) {
-                // Severity only
-                tasks = taskRepository.findAllBySeverityOrderBySeverityAscRegistered(severity);
-            }
-        }
-        else {
-            // device, status, severity
-            if (status != null && severity != null) {
-                tasks = taskRepository.findAllByDeviceIdAndStatusAndSeverityOrderBySeverityAscRegistered(deviceId, status, severity);
-            }
-            else if (status != null) {
-                // Device and status
-                tasks = taskRepository.findAllByDeviceIdAndStatusOrderBySeverityAscRegistered(deviceId, status);
-            }
-            else if (severity != null) {
-                // Device and severity
-                tasks = taskRepository.findAllByDeviceIdAndSeverityOrderBySeverityAscRegistered(deviceId, severity);
-            }
-            else {
-                // Only device
+        switch(method) {
+            case DQ_DEVICEID:
+                // 1: Only device
                 tasks = taskRepository.findAllByDeviceIdOrderBySeverityAscRegistered(deviceId);
-            }
+                break;
+            case DQ_STATUS:
+                // 2: Only status
+                tasks = taskRepository.findAllByStatusOrderBySeverityAscRegistered(status);
+                break;
+            case DQ_DEVICEID + DQ_STATUS:
+                // 3: device and status
+                tasks = taskRepository.findAllByDeviceIdAndStatusOrderBySeverityAscRegistered(deviceId, status);
+                break;
+            case DQ_SEVERITY:
+                // 4: only severity
+                tasks = taskRepository.findAllBySeverityOrderBySeverityAscRegistered(severity);
+                break;
+            case DQ_DEVICEID + DQ_SEVERITY:
+                // 5: device and severity
+                tasks = taskRepository.findAllByDeviceIdAndSeverityOrderBySeverityAscRegistered(deviceId, severity);
+                break;
+            case DQ_STATUS + DQ_SEVERITY:
+                // 6: status and severity
+                tasks = taskRepository.findAllByStatusAndSeverityOrderBySeverityAscRegistered(status, severity);
+                break;
+            case DQ_DEVICEID + DQ_STATUS + DQ_SEVERITY:
+                // 7: everything, device, status, severity
+                tasks = taskRepository.findAllByDeviceIdAndStatusAndSeverityOrderBySeverityAscRegistered(deviceId, status, severity);
+                break;
         }
         return tasks;
     }
