ed5b7411e7ab4b6022e4631a22dfe9f6dadb7b42
SpiritualForest <meditationlover@gmail.com>
2022-09-15 10:27:20 +0300

    Fixed comment on MaintenanceTaskModelAssembler

--- src/main/java/com/etteplan/servicemanual/maintenancetask/MaintenanceTaskModelAssembler.java
+++ src/main/java/com/etteplan/servicemanual/maintenancetask/MaintenanceTaskModelAssembler.java
@@ -13,7 +13,7 @@ import java.util.Map;
 class MaintenanceTaskModelAssembler implements RepresentationModelAssembler<MaintenanceTask, EntityModel<MaintenanceTask>> {
     
     // We need this map because the all() method accepts it as a parameter
-    // Set to public so that the controller can use this field as well, without
+    // Set to protected so that the controller can use this field as well, without
     // having to define its own separate maps for its addHyperlinks function
     protected final Map<String, String> params = new HashMap<String, String>();
 
